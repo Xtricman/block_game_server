d378b23db259cdc11267c20012c239e10647e903
Xtricman <xtricman@gmail.com>
2021-02-08 15:09:08 +0800

    改一下数据结构，直接用JSON

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
 #include <string_view>
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <memory>
+#include "3rd\nlohmann_json\include\nlohmann\json.hpp"
+#include "3rd\nlohmann_json_schema_validator\src\nlohmann\json-schema.hpp"
 
-struct Block {
-    std::u8string_view type_id;
-    std::unordered_map<char8_t *, void *> *interface_table;
-    virtual std::vector<std::uint8_t> serialize() const = 0;
-    virtual ~Block() {};
+using IDType = std::u8string_view;
+using DataType = nlohmann::json;
+using Validator = nlohmann::json_schema::json_validator;
+using UUID = std::tuple<std::uint64_t, std::uint64_t>;
+struct BlockCoordinate {
+    std::uint64_t x,y,z;
 }
-static const std::map<std::u8string_view, std::unique_ptr<Block> (*)(const std::span<std::uint8_t>)> block_desrialize_func_table;
+struct EntityCoordinate {
+    std::uint64_t x,y,z;
+}
+
+static const std::unordered_map<IDType, std::unordered_set<IDType>> block_tag_table;
+static const std::unordered_map<IDType, std::unordered_set<IDType>> entity_tag_table;
+static const std::unordered_map<IDType, std::unordered_set<IDType>> item_tag_table;
+
+static const std::unordered_map<IDType, Validator> block_validator_table;
+static const std::unordered_map<IDType, Validator> entity_validator_table;
+static const std::unordered_map<IDType, Validator> item_validator_table;
 
 struct Entity {
-    std::u8string_view type_id;
-    double x,y,z;
-    std::unordered_map<char8_t *, void *> *interface_table;
-    virtual std::vector<std::uint8_t> serialize() const = 0;
-    virtual ~Entity() {};
+    EntityCoordinate pos;
+    DataType data;
 }
-static const std::map<std::u8string_view, std::unique_ptr<Entity> (*)(const std::span<std::uint8_t>)> entity_desrialize_func_table;
-
-struct Item {
-    std::u8string_view type_id;
-    std::unordered_map<char8_t *, void *> *interface_table;
-    virtual std::vector<std::uint8_t> serialize() const = 0;
-    virtual ~Item() {};
-}
-static const std::map<std::u8string_view, std::unique_ptr<Item> (*)(const std::span<std::uint8_t>)> item_desrialize_func_table;
 
 struct Biome {
     enum class Weather {
@@ -43,4 +45,11 @@ struct Biome {
     std::u8string_view type_id;
     Weather weather;
     Difficulty diffculty;
+    DataType blocks;
+}
+
+struct GameMap {
+    std::unordered_map<BlockCoordinate, Biome> biomes;
+    std::unordered_map<UUID, Entity> entities;
+    std::unordered_map<IDType, DataType> global_data;
 }
