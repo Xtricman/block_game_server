f3851b466491af18d7315bda9e69f9651ae1d39f
Xtricman <xtricman@gmail.com>
2021-02-15 13:22:37 +0800

    不要json schema了，自己做datafixer

--- main.cpp
+++ main.cpp
@@ -1,55 +1,29 @@
+#include <cstdint>
 #include <string_view>
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
-#include <memory>
-#include "3rd\nlohmann_json\include\nlohmann\json.hpp"
-#include "3rd\nlohmann_json_schema_validator\src\nlohmann\json-schema.hpp"
+#include "3rd/nlohmann_json/single_include/nlohmann/json.hpp"
 
-using IDType = std::u8string_view;
+using IDType = std::basic_string_view<char8_t>;
 using DataType = nlohmann::json;
-using Validator = nlohmann::json_schema::json_validator;
-using UUID = std::tuple<std::uint64_t, std::uint64_t>;
-struct BlockCoordinate {
-    std::uint64_t x,y,z;
-}
-struct EntityCoordinate {
-    std::uint64_t x,y,z;
-}
+
+using DataFixer = void (*)(DataType &);
+
+struct BiomeCoordinate {
+    std::uint32_t x,y,z;
+};
 
 static const std::unordered_map<IDType, std::unordered_set<IDType>> block_tag_table;
 static const std::unordered_map<IDType, std::unordered_set<IDType>> entity_tag_table;
 static const std::unordered_map<IDType, std::unordered_set<IDType>> item_tag_table;
 
-static const std::unordered_map<IDType, Validator> block_validator_table;
-static const std::unordered_map<IDType, Validator> entity_validator_table;
-static const std::unordered_map<IDType, Validator> item_validator_table;
-
-struct Entity {
-    EntityCoordinate pos;
-    DataType data;
-}
-
-struct Biome {
-    enum class Weather {
-        Sunny,
-        Rainy,
-        ThunderStorm,
-        Snowy
-    };
-    enum class Difficulty {
-        Easy,
-        Normal,
-        Hard
-    };
-    std::u8string_view type_id;
-    Weather weather;
-    Difficulty diffculty;
-    DataType blocks;
-}
+static const std::unordered_map<IDType, DataFixer> block_fixer_table;
+static const std::unordered_map<IDType, DataFixer> entity_fixer_table;
+static const std::unordered_map<IDType, DataFixer> item_fixer_table;
 
 struct GameMap {
-    std::unordered_map<BlockCoordinate, Biome> biomes;
-    std::unordered_map<UUID, Entity> entities;
-    std::unordered_map<IDType, DataType> global_data;
-}
+    std::unordered_map<BiomeCoordinate, DataType> biomes;
+    DataType global_data;
+};
+
+int main(){}
