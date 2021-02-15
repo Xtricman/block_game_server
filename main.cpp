8aacd513fb2fac988d7c3a004e83f24ee4dbf9fc
Xtricman <xtricman@gmail.com>
2021-02-15 21:02:09 +0800

    更新一下依赖版本，存档结构大大简化，json库不再构建测试用例

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <string_view>
 #include <unordered_map>
 #include <unordered_set>
-#include "3rd/nlohmann_json/single_include/nlohmann/json.hpp"
+#include "thirdparty/nlohmann_json/single_include/nlohmann/json.hpp"
 
-using IDType = std::basic_string_view<char8_t>;
-using DataType = nlohmann::json;
+using IDType = std::u8string_view;
+using Json = nlohmann::json;
 
-using DataFixer = void (*)(DataType &);
+using DataFixer = void (*)(Json &);
 
 struct BiomeCoordinate {
     std::uint32_t x,y,z;
@@ -22,8 +22,8 @@ static const std::unordered_map<IDType, DataFixer> entity_fixer_table;
 static const std::unordered_map<IDType, DataFixer> item_fixer_table;
 
 struct GameMap {
-    std::unordered_map<BiomeCoordinate, DataType> biomes;
-    DataType global_data;
+    std::unordered_map<BiomeCoordinate, Json> biomes;
+    Json global_data;
 };
 
 int main(){}
